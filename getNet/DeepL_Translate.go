db1c5b922adfdac1a63182cd2e68d0707123aff5
AKn <qj.kuan@outlook.com>
2024-12-15 14:05:10 +0800

    新增使用DeepL来汉化，待优化目前只能翻译不带<br>标签的属性

--- getNet/DeepL_Translate.go
+++ getNet/DeepL_Translate.go
@@ -4,19 +4,25 @@ import (
 	"context"
 	"fmt"
 	"github.com/chromedp/chromedp"
-	"time"
 )
 
-func GetTranslate() {
+// 存储结果
+type Trans struct {
+	Index     int
+	Translate string
+	Err       error
+}
+
+func TranslateDeepL(urls []string) []Trans {
 	// 创建一个浏览器上下文
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
 	// 记录开始时间
-	startTime := time.Now().Unix()
+	//startTime := time.Now().Unix()
 
 	// 定义要访问的URL列表
-	urls := []string{
+	/*	urls := []string{
 		"https://www.deepl.com/zh/translator#en/zh-hans/Construct of Terror",
 		"https://www.deepl.com/zh/translator#en/zh-hans/DeathRay Shot",
 		"https://www.deepl.com/zh/translator#en/zh-hans/Dungeon Keeper",
@@ -25,23 +31,15 @@ func GetTranslate() {
 		"https://www.deepl.com/zh/translator#en/zh-hans/Launches The Holder in The Direction They're Facing",
 		"https://www.deepl.com/zh/translator#en/zh-hans/Only those who are real artists can wear this armor.",
 		"https://www.deepl.com/zh/translator#en/zh-hans/Dungeon Keeper say You will get a return crystal upon entry This crystal will let you end your run at any time.",
-	}
+	}*/
 
 	// 定义要获取文本的节点信息
 	nodeInfo := "/html[1]/body[1]/div[1]/div[1]/div[1]/div[3]/div[2]/div[1]/div[2]/div[1]/main[1]/div[2]/nav[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/div[2]/div[3]/section[1]/div[1]/d-textarea[1]/div[1]/p[1]/span[1]"
 
-	// 存储结果
-	//var results []string
-	type result struct {
-		Index     int
-		Translate string
-		Err       error
-	}
-
-	var results []result
+	var results []Trans
 	// 遍历每个URL并执行任务
 	for i, url := range urls {
-		var res result
+		var res Trans
 		res.Index = i
 		err := chromedp.Run(ctx,
 			chromedp.Navigate(url),
@@ -49,13 +47,13 @@ func GetTranslate() {
 		)
 		if err != nil {
 			res.Err = err
-			//fmt.Printf("第 %v 行翻译异常: %v\n", i, err)
+			fmt.Printf("第 %v 行翻译异常: %v\n", i, err)
 			continue
 		}
 		fmt.Printf("翻译结果为: %s\n", res.Translate)
 		results = append(results, res)
 	}
-
+	return results
 	// 计算并输出执行时间
-	fmt.Printf("执行时间: %d 秒\n", time.Now().Unix()-startTime)
+	//fmt.Printf("执行时间: %d 秒\n", time.Now().Unix()-startTime)
 }
---
+++ server/server.go
@@ -0,0 +1,147 @@
+package server
+
+import (
+	"HanHua/getNet"
+	"HanHua/youdaoyunAPI"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+// Result 是对需要翻译的对象的抽象结果集
+type NeedTrans struct {
+	Key string
+	Val string
+}
+
+func HanHuaServer(fileName string) {
+	file, err := os.Open("./File/" + fileName)
+	if err != nil {
+		fmt.Printf("打开文件异常: < %v > \n", err)
+		return
+	}
+	defer func(file *os.File) {
+		err = file.Close()
+		if err != nil {
+			fmt.Printf("关闭文件 < %v > 异常 \n", file.Name())
+		}
+	}(file)
+
+	// 存放待翻译字符
+	var res []NeedTrans
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		//分开后的值的对象 key为 "=" 前的值 val为等于号后的值
+		entry := ParseLine(line)
+		if entry != nil {
+			res = append(res, *entry)
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		fmt.Printf("读取文件异常: %v", err)
+		return
+	}
+
+	DeepLTranslate(res)
+}
+
+// ParseLine 转换并拼接字符
+func ParseLine(line string) *NeedTrans {
+	split := strings.SplitN(line, "=", 2)
+	if len(split) < 2 {
+		// 处理没有 "=" 的情况，这里选择跳过
+		return nil
+	}
+	return &NeedTrans{Key: split[0] + "=", Val: split[1]}
+}
+
+func DeepLTranslate(res []NeedTrans) {
+	urlPre := "https://www.deepl.com/zh/translator#en/zh-hans/"
+	var urls []string
+	for _, re := range res {
+		var url string
+		// 转换标点 因为存在标点符号会让标签变更导致拿不到翻译
+		if strings.Contains(re.Val, ". ") {
+			re.Val = strings.ReplaceAll(re.Val, ". ", ".")
+		}
+		if strings.Contains(re.Val, ", ") {
+			re.Val = strings.ReplaceAll(re.Val, ", ", ",")
+		}
+		if strings.Contains(re.Val, ": ") {
+			re.Val = strings.ReplaceAll(re.Val, ": ", ":")
+		}
+		if strings.Contains(re.Val, "%") {
+			re.Val = strings.ReplaceAll(re.Val, "%", "%25")
+		}
+		/*if strings.Contains(re.Val, "<br>") {
+			re.Val = strings.ReplaceAll(re.Val, "<br>", "")
+		}*/
+		url = urlPre + re.Val
+		urls = append(urls, url)
+	}
+	results := getNet.TranslateDeepL(urls)
+
+	/*for _, url := range urls {
+		fmt.Println(url)
+		time.Sleep(1 * time.Second)
+	}*/
+	for i, result := range results {
+		if result.Err != nil {
+			WriteTranslate(res[i].Key + "此行报错！！！！！！！！！！！！！！！！！！！！！")
+			continue
+		}
+		WriteTranslate(res[i].Key + result.Translate)
+		fmt.Println(res[i].Key + result.Translate)
+	}
+}
+
+// YoudaoTranslate 使用有道云API翻译 需要对应的 appKey 和 appSecret
+func YoudaoTranslate(res []NeedTrans) {
+	//遍历并且输出参数
+	for _, entry := range res {
+		if entry.Val == "" {
+			WriteTranslate(entry.Key)
+			fmt.Println(entry.Key)
+			continue
+		}
+		result := youdaoyunAPI.TransYouDaoYun(entry.Val)
+		WriteTranslate(entry.Key + result)
+		fmt.Println(entry.Key + result)
+
+		// 根据API限制调整睡眠时间 这里用 2 秒翻译一次
+		time.Sleep(2000 * time.Millisecond)
+	}
+}
+
+// WriteTranslate 输出到 zh_CN.lang 文件中
+func WriteTranslate(text string) {
+	out, err := os.Create("./File/zh_CN.lang")
+	if err != nil {
+		fmt.Printf("创建输出文件异常: %v\n", err)
+		return
+	}
+	defer func(out *os.File) {
+		err := out.Close()
+		if err != nil {
+			fmt.Printf("关闭文件 < %v > 异常 \n", out.Name())
+		}
+	}(out)
+
+	writer := bufio.NewWriter(out)
+	defer func(writer *bufio.Writer) {
+		err := writer.Flush()
+		if err != nil {
+			fmt.Printf("Flush 文件 < %v > 异常 \n", out.Name())
+		}
+	}(writer)
+
+	// 输出到文件中
+	_, err = writer.WriteString(text + "\n")
+	if err != nil {
+		fmt.Printf("写入文件异常: %v\n", err)
+	}
+}
